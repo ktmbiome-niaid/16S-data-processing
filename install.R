461d31cbcb94712afcbb6cd6f39556d44a52d5de
Katie McCauley <kathryn.mccauley@nih.gov>
2023-09-20 11:06:21 -0700

    Update Training Docs & Add FASTQ Files

--- install.R
+++ install.R
@@ -1,5 +1,12 @@
 ## This script lists all of the packages that need to be installed
 
-if (!requireNamespace("BiocManager", quietly = TRUE))
+if (!requireNamespace("BiocManager", quietly = TRUE)) {
   install.packages("BiocManager")
+}
+
 BiocManager::install("dada2")
+BiocManager::install("phangorn")
+BiocManager::install("phyloseq")
+BiocManager::install("DECIPHER")
+
+install.packages("ggplot2")
